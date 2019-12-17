27205c9b567869ff15cf1419ffc85a5416a8d4a5
dzhang <dzhang@smsassist.com>
2019-12-17 16:09:11 -0600

    Updates

---
+++ precache-manifest.e6cb1476bdb987e54a496d66156e5fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7a8f5526121ffa7d8cd6a7add4411b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9cfe724d77002235ef23",
+    "url": "/monsters-rolodex/static/css/main.42aef697.chunk.css"
+  },
+  {
+    "revision": "6eee4035cea7dc8cba15",
+    "url": "/monsters-rolodex/static/js/2.0c532c32.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.0c532c32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9cfe724d77002235ef23",
+    "url": "/monsters-rolodex/static/js/main.d31542c5.chunk.js"
+  },
+  {
+    "revision": "202a145f686da1399a48",
+    "url": "/monsters-rolodex/static/js/runtime-main.9f2e98a0.js"
+  }
+]);
